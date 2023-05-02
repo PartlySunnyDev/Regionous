41457dab37412437ebacb4a399ce8fc2afff72fa
Steven Zhu <65844236+steveyxz@users.noreply.github.com>
2023-05-02 20:33:53 +1000

    Added main class

---
+++ src/main/java/me/partlysunny/regionous/Regionous.java
@@ -0,0 +1,46 @@
+package me.partlysunny.regionous;
+
+import me.partlysunny.regionous.handler.RegionManager;
+import me.partlysunny.regionous.hooks.NoWorldGuard;
+import me.partlysunny.regionous.hooks.StandardWorldGuardHook;
+import me.partlysunny.regionous.hooks.WorldGuardHook;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Regionous {
+
+    private static Regionous instance;
+
+    public static Regionous getInstance() {
+        return instance;
+    }
+
+    public static void init(JavaPlugin plugin) {
+        instance = new Regionous(plugin);
+    }
+
+    private final JavaPlugin plugin;
+    private final RegionManager regionManager;
+    private final WorldGuardHook worldGuardHook;
+
+    public Regionous(JavaPlugin plugin) {
+        this.plugin = plugin;
+        this.regionManager = new RegionManager(plugin);
+        if (plugin.getServer().getPluginManager().getPlugin("WorldGuard") != null) {
+            this.worldGuardHook = new StandardWorldGuardHook(plugin);
+        } else {
+            this.worldGuardHook = new NoWorldGuard();
+        }
+    }
+
+    public JavaPlugin getPlugin() {
+        return plugin;
+    }
+
+    public RegionManager getRegionManager() {
+        return regionManager;
+    }
+
+    public WorldGuardHook getWorldGuardHook() {
+        return worldGuardHook;
+    }
+}
